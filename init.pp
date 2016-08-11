312a0a03a2a4dd1723985a696145ea978ba7612b
Tyler Mace <macetw@gmail.com>
2016-08-11 13:38:43 -0400

    Added directives for describing files

--- init.pp
+++ init.pp
@@ -7,13 +7,21 @@ package {'subversion':   ensure => present }
 package {'ntpd':         ensure => present }
 package {'gcc-c++':      ensure => present }
 
+service {'httpd':
+  ensure => running,
+  enable => true,
+  # subscribe => File['...']
+}
+
 user {'macetw':
   ensure           => 'present',
   comment          => 'Tyler Winter Mace',
   password_max_age => '-1',
   password_min_age => '-1',
+  # password         => ... grub-crypt --sha-512
   shell            => '/bin/tcsh',
-  before => File['/home/macetw/.vimrc']
+  before => File['/home/macetw/.vimrc'],
+  managehome => true
 }
 
 file {'vimrc':
@@ -21,6 +29,12 @@ file {'vimrc':
   path => '/home/macetw/.vimrc',
   mode => 600,
   source => 'vimrc'
+  # replace => 'no',
 }
 
-  
+file { '/etc/motd':
+   ensure => file
+   content => 'Count your blessings, not your problems"
+}
+
+
